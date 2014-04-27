3be8c87039dba3724c63585d1319e32738c0003b
Youen Chéné <youen.chene@gadz.org>
2014-04-27 17:28:34 +0200

    Init : basic light & heat support. API side only.

---
+++ README.md
@@ -0,0 +1,21 @@
+## Description
+
+A plugin to control old Tydom 350 from Delta Dore
+
+
+## Quick Start
+
+Donwload the plugin from SARAH's AppStore and follow online plugin's documentation
+
+## Quick API Doc
+
+
+Turn a light 0 on : http://127.0.0.1:8080/sarah/Tydom350?command=LIGHT&item=0&value=on
+
+Turn a light 0 off : http://127.0.0.1:8080/sarah/Tydom350?command=LIGHT&item=0&value=off
+
+
+Turn a heat area on mode : http://127.0.0.1:8080/sarah/Tydom350?command=HEAT&area=3&value=on
+
+Turn a heat area eco mode : http://127.0.0.1:8080/sarah/Tydom350?command=HEAT&area=3&value=eco
+
---
+++ tydom350.js
@@ -0,0 +1,56 @@
+﻿exports.action = function(data, callback, config, SARAH) {
+  config = config.modules.Tydom350;
+  if (!config.tydom_url){
+    callback({'tts' : 'Paramètre tydom_url invalide'});
+    return;
+  }
+
+  if (data.command == "LIGHT") {
+    if (data.item>=0) {
+      var tydomvalue="102";
+      if (data.value=="on") { tydomvalue="101";}
+      sendURL(config.tydom_url+"cgi-bin/Cmd_X2D.cgi?LIGHT="+data.item+"&V="+tydomvalue, callback, function(body){
+        callback({'tts' : 'Ordre lumière envoyé.'}); 
+      });
+   } else {
+    callback({'tts' : 'Paramètre manquant pour l\'ordre.'}); 
+   }
+  } else if (data.command == "HEAT") {
+    if (data.area>=1) 
+    {
+      data.area=data.area-1;
+      var tydomvalue=0;
+      if (data.value=="on") { tydomvalue=3;}
+      sendURL(config.tydom_url+"cgi-bin/Cmd_X2D.cgi?ZONE="+data.area+"&ALL="+tydomvalue, callback, function(body){
+        callback({'tts' : 'Ordre chauffage envoyé.'}); 
+      });
+    } else {
+      callback({'tts' : 'Paramètre manquant pour l\'ordre.'}); 
+    }
+   } else {
+    callback({'tts' : 'Ordre absent ou inconnu.'}); 
+  }
+
+
+
+}
+
+
+
+var sendURL = function(url, callback, cb){
+
+  var request = require('request');
+  request({ 'uri' : url,method: "GET" }, function (err, response, body){
+    
+    if (err || response.statusCode != 200) {
+      console.log("Called URL :" +url);
+      console.log("Status :" +response.statusCode);
+      console.log("Error :" +err);
+      callback({'tts': "L'action a échoué"});
+      return;
+    }
+
+    cb(body);
+  });
+
+} 
